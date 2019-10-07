915689bc9b69c882033146c44b7ae303564a4e9f
samuelyapwoonpin <samuelywp@gmail.com>
2019-10-07 16:34:26 +0800

    Updates

---
+++ precache-manifest.c42d3eaa234ec0b4a24e593bf1761648.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ffefe896d7bcb864888ef4a20c9f6b0",
+    "url": "/note-taker/index.html"
+  },
+  {
+    "revision": "8d63c0e70afbeb11149b",
+    "url": "/note-taker/static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "8d63c0e70afbeb11149b",
+    "url": "/note-taker/static/js/2.5ed25568.chunk.js"
+  },
+  {
+    "revision": "c0ab02746dd4602fda11",
+    "url": "/note-taker/static/js/main.f1e8bb75.chunk.js"
+  },
+  {
+    "revision": "d7af407f361985ba8d01",
+    "url": "/note-taker/static/js/runtime-main.86a4194e.js"
+  }
+]);
